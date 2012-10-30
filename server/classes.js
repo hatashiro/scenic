607718fe229bc4d40ab94e97d42ce9de92f3a72e
HyeonJe Jun <noraesae@yuiazu.net>
2012-10-30 16:00:08 +0900

    Add server side socket functions.
    Add Channel and User classes and add server side socket
    functions using them.

---
+++ server/classes.js
@@ -0,0 +1,87 @@
+var _ = require('underscore')._;
+
+module.exports = {
+    User: function(nick, socket) {
+        this.nick = nick;
+        this.socket = socket;
+        this.channel = null;
+
+        this.join = function(channel) {
+            this.channel = channel;
+            this.channel.addUser(this);
+            this.channel.notice(this.nick+" has joined.");
+            this.socket.emit('channel_joined', {nick:this.nick, channel: channel.model.name});
+            this.channel.updateUserlist();
+        }
+
+        this.say = function(msg) {
+            this.socket.emit('chat', {from: this.nick, msg: msg, from_me: true});
+            this.channel.transmit(msg, from=this);
+        }
+
+        this.changeNick = function(nick) {
+            var old_nick = this.nick;
+            this.nick = nick;
+            this.channel.preventNickDuplication(this);
+            this.channel.notice(old_nick+" is now known as "+this.nick);
+            this.socket.emit('nick_changed', {nick:this.nick});
+            this.channel.updateUserlist();
+        }
+
+        this.disconnect = function() {
+            this.channel.removeUser(this);
+            this.channel.notice(this.nick+" has left.");
+            this.channel.updateUserlist();
+        }
+    },
+
+    Channel: function(model) {
+        this.model = model;
+        this.users = [];
+        this.current_image = null;
+
+        this.addUser = function(user) {
+            this.preventNickDuplication(user);
+            this.users.push(user);
+            user.channel = this;
+        }
+
+        this.removeUser = function(user) {
+            this.users.splice(this.users.indexOf(user), 1);
+        }
+
+        this.preventNickDuplication = function(user) {
+            while(this.nickDuplicated(user.nick)) {
+                user.nick = user.nick + '_';
+            }
+        }
+
+        this.nickDuplicated = function(nick) {
+            var usernicks = [];
+            for(var user in users) {
+                usernicks.put(user.nick);
+            }
+            return (nick in usernicks);
+        }
+
+        this.transmit = function(msg, from) {
+            _.each(this.users, function(user) {
+                if(user != from) {
+                    user.socket.emit('chat', {from: user.nick, msg: msg, from_me: false});
+                }
+            });
+        };
+
+        this.notice = function(msg) {
+            _.each(this.users, function(user) {
+                user.socket.emit('notice', {msg: msg});
+            });
+        };
+
+        this.updateUserlist = function() {
+            _.each(this.users, function(user) {
+                user.socket.emit('update_userlist', {userlist: this.users});
+            });
+        };
+    },
+}
--- server/socket.js
+++ server/socket.js
@@ -1,12 +1,78 @@
 var socketio = require('socket.io'),
-    settings = require('../settings');
+    settings = require('../settings'),
+    mongoose = require('mongoose'),
+    Channel = require('./classes').Channel,
+    User = require('./classes').User;
+
+var ChannelModel = mongoose.model('Channel'),
+    PictureModel = mongoose.model('Picture');
 
 function socket(web) {
     var io = this.io = socketio.listen(web);
     console.log('Socket.io now listening on Express');
 
+    // channels
+    var channels = this.channels = {};
+
     io.sockets.on('connection', function(socket) {
-        socket.emit('hello', {hello: 'world'});
+        var user = null;
+
+        socket.emit('connected', {});
+
+        socket.on('create_user', function(data) {
+            if(!data.nick) {
+                socket.emit('error', {type: 'EmptyNick'});
+                return;
+            }
+
+            user = new User(data.nick, socket);
+            socket.emit('user_created', {});
+        });
+
+        socket.on('join_channel', function(data) {
+            if(!user) {
+                socket.emit('error', {type: 'NoUser'});
+            }
+
+            if(channels[data.channel]) {
+                user.join(channels[data.channel]);
+            }
+            else {
+                ChannelModel.find({name: data.channel}, function(err, channels) {
+                    var channel = null;
+                    if(channels.length) {
+                        channel = channels[0];
+                    }
+                    else {
+                        // create new
+                        channel = new ChannelModel({name: data.channel, pictures: []});
+                        channel.save();
+                    }
+                    channels[data.channel] = new Channel(channel);
+                    user.join(channels[data.channel]);
+                });
+            }
+        });
+
+        socket.on('chat', function(data) {
+            if(!user) {
+                socket.emit('error', {type: 'NoUser'});
+            }
+
+            user.say(data.msg);
+        });
+
+        socket.on('change_nick', function(data) {
+            if(!user) {
+                socket.emit('error', {type: 'NoUser'});
+            }
+
+            user.changeNick(data.nick);
+        });
+
+        socket.on('disconnect', function() {
+            user.disconnect();
+        });
     });
 }
 
