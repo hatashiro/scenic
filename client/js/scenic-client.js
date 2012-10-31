756d46ca2fa9969d3fcf60e71a7e3ed66726a37c
HyeonJe Jun <noraesae@yuiazu.net>
2012-10-31 15:23:57 +0900

    Detach functions from scenic-client.js.
    Functions used in templates will be placed in functions.js.

--- client/js/scenic-client.js
+++ client/js/scenic-client.js
@@ -8,6 +8,7 @@
 //= require 'router.js'
 //= require 'models.js'
 //= require 'socket.js'
+//= require 'functions.js'
 //= require_tree 'views'
 
 $(function() {
@@ -19,26 +20,3 @@ $(function() {
 
     Backbone.history.start();
 });
-
-function join_channel() {
-    var channel_name = $('#channel_name').val();
-
-    if(channel_name[0] == '#') {
-        channel_name = channel_name.substring(1);
-    }
-
-    if(!channel_name) {
-        return false;
-    }
-
-    // check alphanumerics
-    var alphanumerics = /^[A-Za-z0-9_]+$/;
-    if(!channel_name.match(alphanumerics)) {
-        alert('Sorry! A channel name can only contains alphanumerics and underscore.');
-        return false;
-    }
-
-    location.href = '#' + channel_name;
-
-    return false;
-}
