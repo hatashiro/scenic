e26de8e7a7e08a9f4ec7f248b434aacd1301b3d8
HyeonJe Jun <noraesae@yuiazu.net>
2012-10-31 16:46:11 +0900

    Fix firefox esc key problem.
    On firefox, esc key make the websocket disconnected.

--- client/js/utils.js
+++ client/js/utils.js
@@ -1,2 +1,7 @@
 if(!console) var console = {}
 if(!console.log) console.log = function(msg) { /* do nothing */ }
+
+$(function() {
+    // fix firefox socket.io disconnected problem
+    window.addEventListener('keydown', function(e) { (e.keyCode == 27 && e.preventDefault()) })
+});
