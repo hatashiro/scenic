42f102dd7bc02b7cd96c634e80e25af6890ebcc7
Hyeonje Alex Jun <me@noraesae.net>
2012-11-14 23:19:13 +0900

    Main script can contain initializing process.
    scenic.js has a waterfall-flow initializing process.

--- scenic.js
+++ scenic.js
@@ -1,4 +1,28 @@
-var Scenic = require('./server/scenic-server');
+var Scenic = require('./server/scenic-server'),
+    path = require('path'),
+    fs = require('fs');
 
-var scenic_app = new Scenic();
-scenic_app.run();
+function initialize(processes) {
+    var process_index = 0;
+    var iterator = function() {
+        return function() {
+            var func = processes[process_index];
+            if(!func) return;
+            process_index += 1;
+            func(iterator());
+        };
+    };
+    iterator()();
+}
+
+initialize([
+    function make_uploads_dir(next) {
+        fs.mkdir(path.join(__dirname, 'uploads'), function(err) {
+            next();
+        });
+    },
+    function run() {
+        var scenic_app = new Scenic();
+        scenic_app.run();
+    }
+]);
