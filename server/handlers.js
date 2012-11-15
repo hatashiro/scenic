8e5e57db98ae60f580913e9fd546c24b786a7483
Hyeonje Alex Jun <me@noraesae.net>
2012-11-15 22:05:51 +0900

    Fix the bug that fail to create thumbnail of gif file format.
    There is the bug in node-gm that it can't parse the size
    of gif properly. Thus if the file is gif, convert it to jpg
    and then get size.

--- server/handlers.js
+++ server/handlers.js
@@ -68,53 +68,79 @@ function Handlers(web) {
 
                             picture.original = original;
 
-                            // get size
-                            gm(original).size(function(err, size) {
-                                // create thumbnail
-                                var thumbnail_width = size.width >= size.height ? 300 : size.width * 300 / size.height,
-                                    thumbnail_height = size.width <= size.height ? 300 : size.height * 300 / size.width,
-                                    thumbnail = path.join(picture_dir, "thumbnail.jpg");
+                            var createThumbnailAndMinified = function() {
+                                // get size
+                                gm(original).size(function(err, size) {
+                                    // create thumbnail
+                                    var thumbnail_width = size.width >= size.height ? 300 : size.width * 300 / size.height,
+                                        thumbnail_height = size.width <= size.height ? 300 : size.height * 300 / size.width,
+                                        thumbnail = path.join(picture_dir, "thumbnail.jpg");
 
-                                gm(original).resize(thumbnail_width, thumbnail_height).quality(90).write(thumbnail, function(err) {
+                                    gm(original).resize(thumbnail_width, thumbnail_height).quality(90).write(thumbnail, function(err) {
+                                        if(err) {
+                                            response('thumbnail_error:'+err);
+                                            return;
+                                        }
+
+                                        picture.thumbnail = thumbnail;
+
+                                        var save = function() {
+                                            picture.upload = Date.now();
+                                            picture.save(function(err) {
+                                                if(err) {
+                                                    response('save_err:'+err);
+                                                    return;
+                                                }
+
+                                                if(ext === '.gif') {
+                                                    // remove temp jpg file
+                                                    fs.unlink(original, function(err) {
+                                                        response('success');
+                                                    });
+                                                }
+                                                else {
+                                                    response('success');
+                                                }
+                                            });
+                                        };
+
+                                        // if the image is larger than 1mb or 1920x1080, create minified image.
+                                        if((image.size > 1024 * 1024 || size.width > 1920 || size.height > 1080) && ext !== '.gif') {
+                                            var minified_width = (size.width / size.height) >= (16 / 9) ? 1920 : 1080 * size.width / size.height,
+                                                minified_height = (size.width / size.height) <= (16 / 9) ? 1080 : 1920 * size.height / size.width,
+                                                minified = path.join(picture_dir, "minified.jpg");
+
+                                            gm(original).resize(minified_width, minified_height).quality(90).write(minified, function(err) {
+                                                if(err) {
+                                                    response('minified_error:'+err);
+                                                    return;
+                                                }
+
+                                                picture.minified = minified;
+                                                save();
+                                            });
+                                        }
+                                        else {
+                                            save();
+                                        }
+                                    });
+                                });
+                            };
+
+                            if(ext !== '.gif') {
+                                createThumbnailAndMinified();
+                            }
+                            else {
+                                var gif = original;
+                                original = 'temp.jpg';
+                                gm(gif).write(original, function(err) {
                                     if(err) {
-                                        response('thumbnail_error:'+err);
+                                        response('gif_convert_error:'+err);
                                         return;
                                     }
-
-                                    picture.thumbnail = thumbnail;
-
-                                    var save = function() {
-                                        picture.upload = Date.now();
-                                        picture.save(function(err) {
-                                            if(err) {
-                                                response('save_err:'+err);
-                                            }
-
-                                            response('success');
-                                        });
-                                    };
-
-                                    // if the image is larger than 1mb or 1920x1080, create minified image.
-                                    if(image.size > 1024 * 1024 || size.width > 1920 || size.height > 1080) {
-                                        var minified_width = (size.width / size.height) >= (16 / 9) ? 1920 : 1080 * size.width / size.height,
-                                            minified_height = (size.width / size.height) <= (16 / 9) ? 1080 : 1920 * size.height / size.width,
-                                            minified = path.join(picture_dir, "minified.jpg");
-
-                                        gm(original).resize(minified_width, minified_height).quality(90).write(minified, function(err) {
-                                            if(err) {
-                                                response('minified_error:'+err);
-                                                return;
-                                            }
-
-                                            picture.minified = minified;
-                                            save();
-                                        });
-                                    }
-                                    else {
-                                        save();
-                                    }
+                                    createThumbnailAndMinified();
                                 });
-                            });
+                            }
                         });
                     });
                 });
